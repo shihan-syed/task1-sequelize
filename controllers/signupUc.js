1e1085a3ad82c322f38b769e05f78c1570e87af0
SHIHAN SYED <113380225+shihan-syed@users.noreply.github.com>
2022-09-21 00:10:49 +0530

    Update signupUc.js

--- controllers/signupUc.js
+++ controllers/signupUc.js
@@ -3,21 +3,7 @@ const JWT=require('jsonwebtoken')
 const bcrypt=require('bcrypt')
 
 
-const privateKey=`-----BEGIN RSA PRIVATE KEY-----
-MIICXAIBAAKBgQCcXFatcPAM+VrQA2/gTUYChZ9fxEtQGYsrB6KPNcIPlznfCGEr
-fX/+0W58JZKOx6ZUQDtSBAtjg5xyp8eQb6xGCR69flrrupen2BbVVPIeT/hXOkX1
-nSRyTUVBLAqtwzPegjFo0oPlt5bjHD9g5Meqm8SW+o9UY+G/RKqEYycGPQIDAQAB
-AoGAPMlRESBk0kZmXZPYfHTOENDtwodHWB52EgZn1hDqHrGF2AUj8Ypapi+5Kh5Z
-OZS4PuyxoxLbcpaJzbEYTjh72irEjBmKUxmaWEFvdc5CUjTBUQkUMR7HH6EsC/px
-fQmtCZ3sCuTIyoTTKJxZky/jM1qWrzlAm5YdJb8DO/o35ykCQQDJzI8BXuqqoOuw
-dWfMIQJ13pNwisbefEAUaNL+PicZMhHos0fiXvi/ROt9rGk5dG6Bz5vusJ1ecibW
-tgTuBoi3AkEAxlt/UAF73krq5lefrRvHrhiAONk0JAmoFLEh7j3Kfuad4yrNrEi0
-AJpWefUuHTQ26za8Y23O99gDsvlkLEfsqwJAayFVqfyhyAO2G++4Mv4zOC/5Rf3m
-paaAvlUH7kFAsHeMWmUS/HI+zAvimM0273GIWRHGTmkkokcnp0TqSRLe8QJABOmJ
-KpsOzT9/t7qdyrSwakBTFJVUdDwE/JYyZ+sB08i0SkbqFwUp6AXqdFS9Ep4TahnN
-6oxynrKrzMfeUjZpxwJBAMeiLQ/r5gFzU4Ffr+DwoX7uVGW3jSeTB/AM/MX4gKKT
-ESi2bqMyigZRinVTnsmGuwDvromR2c+JmC3ZlB1WMQ0=
------END RSA PRIVATE KEY-----`;
+const privateKey=`put rsa private key from online`;
 
 
 //signup
